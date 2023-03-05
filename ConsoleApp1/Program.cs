018bdd7e1a7b9cb919b5e1e292904e597b15477d
elnurumo <elnurqasimov592@gmail.com>
2023-03-05 23:28:07 +0400

    change bool name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-//bool flag = true;
-//int increment = 0;
-//Console.WriteLine("Please enter the size of the array");
-//int sizeArray = Convert.ToInt32(Console.ReadLine());
-//int[] numbers = new int[0];
-//do
-//{
-//    Console.WriteLine("Enter element ");
-//    int element = Convert.ToInt32(Console.ReadLine());
-//    Console.WriteLine(element);
-//    numbers[increment] = element;
-//    increment++;
-//    Console.WriteLine("Do you want to continue? ");
-//    string answer = Console.ReadLine();
-//    if (!(answer == "y" || answer == "Y"))
-//    {
-//        flag = false;
-//        Console.WriteLine(".......exiting");
-//    }
-
-//} while (flag);
-
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         string[] employees = new string[0];
-        bool isContinuing = true;
+        bool flag = true;
 
         do
         {
@@ -42,8 +18,8 @@ class Program
             Console.Write("Do you want to continue? (yes/no): ");
             string answer = Console.ReadLine();
 
-            isContinuing = (answer.ToLower() == "yes");
-        } while (isContinuing);
+            flag = (answer.ToLower() == "yes");
+        } while (flag);
 
         Console.WriteLine("Employees:");
         foreach (string employee in employees)
@@ -51,6 +27,6 @@ class Program
             Console.WriteLine(employee);
         }
 
-        Console.ReadLine();
+        //Console.ReadLine();
     }
 }
